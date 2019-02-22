b81ba96182e8031e6a9366ff941f12a6a18c072f
Theofanis Despoudis <theo.despoudis@teckro.com>
2019-02-22 20:47:02 +0000

    Create {{cookiecutter.project_slug}}Tests.swift

---
+++ {{cookiecutter.project_slug}}/Tests/{{cookiecutter.project_slug}}Tests/{{cookiecutter.project_slug}}Tests.swift
@@ -0,0 +1,15 @@
+import Foundation
+import XCTest
+import {{cookiecutter.project_slug}}
+
+class {{cookiecutter.project_slug}}Tests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual({{cookiecutter.project_slug}}().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}

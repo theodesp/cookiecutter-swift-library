2244b3aa4775de3ca46e0c61a8ad0ea6dfdcce6c
Theo <theo.despoudis@teckro.com>
2019-02-22 16:44:45 +0000

    Package Fixes

--- {{cookiecutter.project_slug}}/Package.swift
+++ {{cookiecutter.project_slug}}/Package.swift
@@ -1,5 +1,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "{{ cookiecutter.project_slug }}")
-    
+    name: "{{ cookiecutter.project_slug }}"
+     products: [
+      .library(name: "{{ cookiecutter.project_slug }}", targets: ["{{ cookiecutter.project_slug }}"])
+    ],
+    targets: [
+      .target(name: "{{ cookiecutter.project_slug }}", dependencies: []),
+      .testTarget(name: "{{ cookiecutter.project_slug }}Tests", dependencies: ["{{ cookiecutter.project_slug }}"]),
+   ])
+   
